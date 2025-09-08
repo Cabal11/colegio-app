8f13cee8420a03632b3f880abf57dc5c4da7e6c8
cabal <cabal@ATHENA>
2025-09-07 23:08:04 -0600

    Creado menu para moviles y link en logo navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,25 +1,40 @@
-import React from "react";
-import "@/app/styles/Navbar.css"
+"use client";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "@/app/styles/Navbar.css";
 import Link from "next/link";
 
 function Navbar() {
+  const [menuAbierto, setMenuAbierto] = useState(false);
+  const cerrarMenu = () => {
+    setMenuAbierto(false);
+  }
+
   return (
     <header>
+      <Link href="/">
       <img className="img-nav" alt="Icono" src="/foto1.png" />
-      <nav className="nav-bar">
-        
+      </Link>
+      
+      <button
+        className="menu-cambiar"
+        onClick={() => setMenuAbierto(!menuAbierto)}
+      >
+        {menuAbierto ? <FaTimes style={{color: 'black'}} /> : <FaBars style={{color: 'black'}} />}
+      </button>
+      <nav className={`nav-bar ${menuAbierto ? 'abierto' : ''}`}>
         <ol className="nav-items">
           <li className="nav-Inicio">
-            <Link href="/">Inicio</Link>
+            <Link href="/" onClick={cerrarMenu}>Inicio</Link>
           </li>
           <li className="nav-Matricula">
-            <Link href="/matricula">Matricula</Link>
+            <Link href="/matricula" onClick={cerrarMenu}>Matricula</Link>
           </li>
           <li className="nav-Vida">
-            <Link href="/vida-estudiantil">Vida estudiantil</Link>
+            <Link href="/vida-estudiantil" onClick={cerrarMenu}>Vida estudiantil</Link>
           </li>
           <li className="nav-Contacto">
-            <Link href="/contacto">Contacto</Link>
+            <Link href="/contacto" onClick={cerrarMenu}>Contacto</Link>
           </li>
         </ol>
       </nav>
