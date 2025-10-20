a65dc51a53a89bb215a89206494e0880d7bb05a6
cabal <cabal@ATHENA>
2025-10-20 15:57:31 -0600

    config para vercel proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async proxyInverso() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://backend-nodejs-production-79b3.up.railway.app/:path*",
+      },
+      {
+        source: "/ping",
+        destination:
+          "https://backend-nodejs-production-79b3.up.railway.app/ping",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
