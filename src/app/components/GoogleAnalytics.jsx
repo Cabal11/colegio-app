f4278bbaa09b1af3ad8ea766a388c2a5264c3cab
cabal <cabal@ATHENA>
2025-10-27 14:00:46 -0600

    modified GoogleAnalytics.jsx

--- src/app/components/GoogleAnalytics.jsx
+++ src/app/components/GoogleAnalytics.jsx
@@ -1,26 +1,27 @@
-"use client";
+'use client';
 
 import Script from "next/script";
 
 export default function GoogleAnalytics() {
+  const GA_ID = "G-2T0GPVTL7Q";
+
   return (
     <>
+      {/* Carga del script principal de GA */}
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-2T0GPVTL7Q"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
         strategy="afterInteractive"
       />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-2T0GPVTL7Q');
-          `,
-        }}
-      />
+
+      {/* Configuración de Google Analytics */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
     </>
   );
 }
